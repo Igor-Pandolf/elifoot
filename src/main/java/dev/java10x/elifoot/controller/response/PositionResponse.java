89563ec1d72cd53bfd2d206b1ca480452a992269
igorpandolf <pandolf.igor@gmail.com>
2026-07-29 11:42:38 -0300

    Positions

---
+++ src/main/java/dev/java10x/elifoot/controller/response/PositionResponse.java
@@ -0,0 +1,15 @@
+package dev.java10x.elifoot.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PositionResponse {
+    private String value;
+    private String label;
+}
